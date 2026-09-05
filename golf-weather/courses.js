d7a80f9907ca2d4cb5054f8f5c1c0e3bde69f5d1
kjpia21-design <kjpia21@gmail.com>
2026-09-05 07:03:46 -0700

    expand courses: full 수도권 coverage + more nationwide

--- golf-weather/courses.js
+++ golf-weather/courses.js
@@ -1,1 +1,5 @@
-PLACEHOLDER_WILL_REPLACE
+/** OpenStreetMap leisure=golf_course 좌표 (대한민국) — 수도권 보강 */
+import { PART1 } from "./courses-part1.js";
+import { PART2 } from "./courses-part2.js";
+import { PART3 } from "./courses-part3.js";
+export const COURSES = [...PART1, ...PART2, ...PART3];
